e6ee94cab546f77b6bd5e0c9078c9f61e63fcc74
Thomas Lindblom <thomas.lindblom.ctr@friends.dds.mil>
2020-11-13 20:41:48 -0800

    add shell of toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { BasicInfoComponent } from './basic-info/basic-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    BasicInfoComponent
   ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        MatSliderModule
+        MatSliderModule,
+        MatToolbarModule,
+        MatIconModule,
     ],
   providers: [],
   bootstrap: [AppComponent]
